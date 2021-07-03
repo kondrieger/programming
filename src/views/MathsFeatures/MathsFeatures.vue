ea19734285c5643513a932f5c4838584d84c2704
kondrieger <kondrieger@gmail.com>
2021-07-04 00:40:28 +0300

    Настроил свайпер и добавил его во второй экран математики

--- src/views/MathsFeatures/MathsFeatures.vue
+++ src/views/MathsFeatures/MathsFeatures.vue
@@ -1,129 +1,100 @@
 <template>
     <div class="container features">
-        <div v-if="!isTablet" class="features__item">
-            <p class="features__item-text"><b>Занятия с преподавателем</b><br />в группах до 12 человек</p>
-            <img class="features__item-img" :src="require('../../img/svg/feature_1.svg')" alt="" />
-        </div>
-        <div v-if="!isTablet" class="features__item">
-            <p class="features__item-text"><b>В школе Алгоритмики</b><br />или онлайн</p>
-            <img class="features__item-img" :src="require('../../img/svg/feature_2.svg')" alt="" />
-        </div>
-        <div v-if="!isTablet" class="features__item">
-            <p class="features__item-text"><b>Курсы для всех</b><br />мы не смотрим на школьные оценки</p>
-            <img class="features__item-img" :src="require('../../img/svg/feature_3.svg')" alt="" />
-        </div>
+        <ul v-if="!isMobileLg" class="features__list">
+            <li class="features__list-item">
+                <p class="features__list-item-text"><b>Занятия с преподавателем</b><br />в группах до 12 человек</p>
+                <img class="features__list-item-img" :src="require('../../img/svg/feature_1.svg')" alt="" />
+            </li>
+            <li class="features__list-item">
+                <p class="features__list-item-text"><b>В школе Алгоритмики</b><br />или онлайн</p>
+                <img class="features__list-item-img" :src="require('../../img/svg/feature_2.svg')" alt="" />
+            </li>
+            <li class="features__list-item">
+                <p class="features__list-item-text"><b>Курсы для всех</b><br />мы не смотрим на школьные оценки</p>
+                <img class="features__list-item-img" :src="require('../../img/svg/feature_3.svg')" alt="" />
+            </li>
+        </ul>
+        <swiper v-else ref="mySwiper" class="" :options="swiperOptions">
+            <swiper-slide>
+                <div class="features__list-item">
+                    <p class="features__list-item-text"><b>Занятия с преподавателем</b><br />в группах до 12 человек</p>
+                    <img class="features__list-item-img" :src="require('../../img/svg/feature_1.svg')" alt="" /></div
+            ></swiper-slide>
+            <swiper-slide>
+                <div class="features__list-item">
+                    <p class="features__list-item-text"><b>В школе Алгоритмики</b><br />или онлайн</p>
+                    <img class="features__list-item-img" :src="require('../../img/svg/feature_2.svg')" alt="" /></div
+            ></swiper-slide>
+            <swiper-slide>
+                <div class="features__list-item">
+                    <p class="features__list-item-text"><b>Курсы для всех</b><br />мы не смотрим на школьные оценки</p>
+                    <img class="features__list-item-img" :src="require('../../img/svg/feature_3.svg')" alt="" /></div
+            ></swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
     </div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper';
+import 'swiper/css/swiper.css';
+
+import './styles.css';
 export default {
     name: 'MathFeatures',
-
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    directives: {
+        swiper: directive,
+    },
+    data() {
+        return {
+            swiperOptions: {
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+            },
+        };
+    },
     computed: {
-        isTablet() {
-            return this.$mq.tablet;
+        isMobileLg() {
+            return this.$mq.mobileLg;
         },
     },
 };
 </script>
 
 <style>
-.features {
+.swiper-container {
+    height: 300px;
+    width: 100%;
+}
+
+.swiper-slide {
+    text-align: center;
+    font-size: 38px;
+    font-weight: 700;
+    background-color: transparent;
+    display: -webkit-box;
+    display: -ms-flexbox;
     display: flex;
-    justify-content: space-between;
-    padding: calc(var(--gs) * 6) 0;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+}
 
-    @media (--tablet) {
-        flex-direction: column;
-        justify-content: unset;
-    }
+.swiper-pagination-bullet {
+    background-color: var(--cl-violet);
+    opacity: 1;
+}
 
-    @media (--mobile-lg) {
-        display: block;
-    }
-
-    &__item {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        max-width: 300px;
-
-        @media (--desktop) and (--tablet-min) {
-            max-width: 259px;
-        }
-
-        @media (--tablet) {
-            flex-direction: row;
-            justify-content: unset;
-        }
-
-        &:nth-child(1) {
-            @media (--tablet) {
-                margin-left: calc(var(--gs) * 2);
-                p {
-                    min-width: 250px;
-                }
-            }
-        }
-        &:nth-child(2) {
-            @media (--desktop) and (--tablet-min) {
-                max-width: 190px;
-            }
-            @media (--tablet) {
-                margin-left: calc(var(--gs) * 19);
-
-                p {
-                    min-width: 225px;
-                }
-            }
-        }
-        &:nth-child(3) {
-            @media (--tablet) {
-                margin-left: calc(var(--gs) * 0);
-            }
-            img {
-                margin-left: -100px;
-                @media (--desktop) and (--tablet-min) {
-                    margin-left: -80px;
-                }
-
-                @media (--tablet) {
-                    margin-left: 0;
-                }
-            }
-            p {
-                margin-bottom: calc(var(--gs) * 2.5);
-                @media (--tablet) {
-                    min-width: 319px;
-                }
-            }
-        }
-        &:nth-child(1),
-        &:nth-child(3) {
-            @media (--tablet) {
-                img {
-                    order: -1;
-                }
-            }
-        }
-
-        &-text {
-            font-size: 20px;
-            margin-bottom: calc(var(--gs) * 4.5);
-
-            @media (--desktop) {
-                font-size: 18px;
-            }
-
-            @media (--tablet) {
-                font-size: 16px;
-                margin-bottom: 0 !important;
-            }
-        }
-
-        &-img {
-            max-height: 140px;
-        }
-    }
+.swiper-pagination-bullet-active {
+    border: 1px solid var(--cl-violet);
+    background-color: #fff;
 }
 </style>
