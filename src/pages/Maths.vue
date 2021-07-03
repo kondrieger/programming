8a46e75b826fe27141bddaf0869018fe34dd9cf6
kondrieger <kondrieger@gmail.com>
2021-07-04 02:30:38 +0300

    Сделал этот сложный фон

--- src/pages/Maths.vue
+++ src/pages/Maths.vue
@@ -3,6 +3,7 @@
         <Header />
         <MathsStartBlock />
         <MathFeatures />
+        <MathsWorld />
     </div>
 </template>
 
@@ -10,10 +11,48 @@
 import Header from '../components/header/header.vue';
 import MathsStartBlock from '../views/MathsStartBlock/MathsStartBlock.vue';
 import MathFeatures from '../views/MathsFeatures/MathsFeatures.vue';
+import MathsWorld from '../views/MathsWorld/MathsWorld.vue';
 
 export default {
     name: 'Maths',
 
-    components: { Header, MathsStartBlock, MathFeatures },
+    components: { Header, MathsStartBlock, MathFeatures, MathsWorld },
 };
 </script>
+
+<style scoped>
+.maths {
+    position: relative;
+
+    &::before {
+        content: '';
+        position: absolute;
+        top: -30px;
+        width: 100%;
+        width: 100%;
+        height: 4315.69px;
+        background-image: svg-load('../img/svg/math_bg.svg');
+        background-repeat: no-repeat;
+        background-position: center center;
+        z-index: -100;
+
+        @media (--desktop-lg) {
+            height: 3215.69px;
+            background-position: center 33px;
+        }
+
+        @media (--desktop) {
+            background-position: center 73px;
+        }
+
+        @media (--tablet) {
+            background-position: center -206px;
+        }
+
+        @media (--mobile-lg) {
+            background-position: center 57px;
+            background-image: svg-load('../img/svg/math_bg_sm.svg');
+        }
+    }
+}
+</style>
---
+++ src/views/MathsWorld/MathsWorld.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="world__wrap">
+        <div class="container">
+            <div class="world">
+                <h2 class="world__title">Учитесь в школе международного уровня</h2>
+                <img class="world__map" :src="require('../../img/svg/map_white.svg')" alt="" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.world {
+    padding: calc(var(--gs) * 8) 0;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+
+    &__wrap {
+        position: relative;
+        width: 100%;
+        background: transparent;
+        display: flex;
+    }
+
+    &__title {
+        margin: 0 auto;
+        max-width: 80%;
+        padding-bottom: calc(var(--gs) * 6.5);
+    }
+}
+</style>
