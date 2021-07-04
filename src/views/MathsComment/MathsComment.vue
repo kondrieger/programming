8094a60818d199d758e80e5ddb50d0083035efec
kondrieger <kondrieger@gmail.com>
2021-07-04 22:08:51 +0300

    Доделал блок "В алгоритмике"

--- src/views/MathsComment/MathsComment.vue
+++ src/views/MathsComment/MathsComment.vue
@@ -1,48 +1,70 @@
 <template>
     <div class="container comment">
         <div class="comment__wrap">
-            <h2 class="comment__title">С Алгоритмикой ребенок:</h2>
+            <h2 class="comment__title">В Алгоритмике:</h2>
             <ul v-if="!isTablet" class="comment__list">
                 <li class="comment__list-item">
-                    <img class="comment__list-item-img" :src="require('../../img/svg/math_comment_1.svg')" alt="" />
-                    <p class="comment__list-item-text">Сформирует прочную базу знаний по математике</p>
+                    <img
+                        class="comment__list-item-img"
+                        :src="require('../../img/svg/programming_comment_1.svg')"
+                        alt=""
+                    />
+                    <p class="comment__list-item-text">
+                        Ребенок влюбится в точные науки и по-новому посмотрит на школьные предметы
+                    </p>
                 </li>
                 <li class="comment__list-item comment__list-item--narrow">
-                    <img class="comment__list-item-img" :src="require('../../img/svg/math_comment_2.svg')" alt="" />
-                    <p class="comment__list-item-text">Влюбится в предмет и углубит знания</p>
+                    <div class="comment__list-item-img-wrap">
+                        <img :src="require('../../img/svg/programming_comment_2.svg')" alt="" />
+                    </div>
+                    <p class="comment__list-item-text">
+                        Найдет себя в одном из перспективных IT-направлений — от геймдева до машинного обучения
+                    </p>
                 </li>
                 <li class="comment__list-item comment__list-item--narrow">
-                    <img class="comment__list-item-img" :src="require('../../img/svg/math_comment_3.svg')" alt="" />
-                    <p class="comment__list-item-text">Улучшит успеваемость в школе</p>
-                </li>
-                <li class="comment__list-item">
-                    <img class="comment__list-item-img" :src="require('../../img/svg/math_comment_4.svg')" alt="" />
-                    <p class="comment__list-item-text">Расширит возможности выбора профессии</p>
+                    <img
+                        class="comment__list-item-img"
+                        :src="require('../../img/svg/programming_comment_3.svg')"
+                        alt=""
+                    />
+                    <p class="comment__list-item-text">
+                        Соберет портфолио разработчика из собственных игр, сайтов и приложений
+                    </p>
                 </li>
             </ul>
             <swiper v-else ref="mySwiper" class="" :options="swiperOptions">
                 <swiper-slide>
                     <div class="comment__list-item">
-                        <img class="comment__list-item-img" :src="require('../../img/svg/math_comment_1.svg')" alt="" />
-                        <p class="comment__list-item-text">Сформирует прочную базу знаний по математике</p>
+                        <img
+                            class="comment__list-item-img"
+                            :src="require('../../img/svg/programming_comment_1.svg')"
+                            alt=""
+                        />
+                        <p class="comment__list-item-text">
+                            Ребенок влюбится в точные науки и по-новому посмотрит на школьные предметы
+                        </p>
                     </div>
                 </swiper-slide>
                 <swiper-slide>
                     <div class="comment__list-item">
-                        <img class="comment__list-item-img" :src="require('../../img/svg/math_comment_2.svg')" alt="" />
-                        <p class="comment__list-item-text">Влюбится в предмет и углубит знания</p>
+                        <div class="comment__list-item-img-wrap">
+                            <img :src="require('../../img/svg/programming_comment_2.svg')" alt="" />
+                        </div>
+                        <p class="comment__list-item-text">
+                            Найдет себя в одном из перспективных IT-направлений — от геймдева до машинного обучения
+                        </p>
                     </div>
                 </swiper-slide>
                 <swiper-slide>
                     <div class="comment__list-item">
-                        <img class="comment__list-item-img" :src="require('../../img/svg/math_comment_3.svg')" alt="" />
-                        <p class="comment__list-item-text">Улучшит успеваемость в школе</p>
-                    </div>
-                </swiper-slide>
-                <swiper-slide>
-                    <div class="comment__list-item">
-                        <img class="comment__list-item-img" :src="require('../../img/svg/math_comment_4.svg')" alt="" />
-                        <p class="comment__list-item-text">Расширит возможности выбора профессии</p>
+                        <img
+                            class="comment__list-item-img"
+                            :src="require('../../img/svg/programming_comment_3.svg')"
+                            alt=""
+                        />
+                        <p class="comment__list-item-text">
+                            Соберет портфолио разработчика из собственных игр, сайтов и приложений
+                        </p>
                     </div>
                 </swiper-slide>
                 <div class="swiper-pagination" slot="pagination"></div>
